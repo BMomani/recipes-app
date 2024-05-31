36602766e4bd8488851b29eea5641035714649bf
BMomani <basheer@adnocdistribution.ae>
2024-05-31 20:08:10 +0300

    :closed_lock_with_key: fix config

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://spoonacular.com/food-api/docs
 
 const API_URL = "https://api.spoonacular.com/";
-const API_KEY = '3a1c2b91ed0e4801b742eec062beeae7';//'6c15e8e5ae6c4c529e9b417e0620524e';//process.env.REACT_APP_API_KEY;
+const API_KEY = 'YOUR_API_KEY';//process.env.REACT_APP_API_KEY;
 
 const SEARCH_BASE_URL = `${API_URL}recipes/complexSearch?apiKey=${API_KEY}&query=`;
 const POPULAR_BASE_URL = `${API_URL}recipes/complexSearch?apiKey=${API_KEY}&sort=random`;
